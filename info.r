64235cfbd364e1c126c344d76b1d1dd00d9f7182
ingileif <ingileif@berkeley.edu>
2015-03-03 09:10:20 -0800

    info.r

--- info.r
+++ info.r
@@ -1,32 +1,5 @@
-
-R version 3.1.2 (2014-10-31) -- "Pumpkin Helmet"
-Copyright (C) 2014 The R Foundation for Statistical Computing
-Platform: x86_64-apple-darwin13.4.0 (64-bit)
-
-R is free software and comes with ABSOLUTELY NO WARRANTY.
-You are welcome to redistribute it under certain conditions.
-Type 'license()' or 'licence()' for distribution details.
-
-  Natural language support but running in an English locale
-
-R is a collaborative project with many contributors.
-Type 'contributors()' for more information and
-'citation()' on how to cite R or R packages in publications.
-
-Type 'demo()' for some demos, 'help()' for on-line help, or
-'help.start()' for an HTML browser interface to help.
-Type 'q()' to quit R.
-
-[R.app GUI 1.65 (6833) x86_64-apple-darwin13.4.0]
-
-[History restored from /Users/janeseung/.Rapp.history]
-
-> name = "Jane Seung"
-> SID = "24772895"
-> email = "jseung@berkeley.edu"
-> github_name = "jseung"
-> status = "enrolled"
-> $ git status
-Error: unexpected '$' in "$"
-2015-01-23 11:46:29.408 R[7656:905250] *** WARNING: Method userSpaceScaleFactor in class NSWindow is deprecated on 10.7 and later. It should not be used in new applications. Use convertRectToBacking: instead. 
-> 
+name = "yourName"
+SID = "yourSID"
+email = "email@berkeley.edu"
+github_name = "github_name"
+status = "enrolled/audit/concurrent"
